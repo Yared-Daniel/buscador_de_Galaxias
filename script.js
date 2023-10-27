6a9785588e0c00aa1f55688b63aec006c897e450
Yared-Daniel <141077128+Yared-Daniel@users.noreply.github.com>
2023-10-27 02:19:52 -0300

    code challenge
    metodo post

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
+   const form = document.getElementById('registro-form');
 
-const form = document.getElementById('registro-form');
     form.addEventListener('submit', (event) => {
     event.preventDefault();
+
 const url = 'https://jsonplaceholder.typicode.com/users';
+
 const data = {
     nombre: form.elements['nombre'].value,
     apellido: form.elements['apellido'].value,
     fechaNacimiento: form.elements['fecha-nacimiento'].value
       };
+      
 const options = {
         method: 'POST',
         headers: {
