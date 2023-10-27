4a1bb426b62686c6eb568fffdc85cafd14e8d990
Yared-Daniel <141077128+Yared-Daniel@users.noreply.github.com>
2023-10-27 02:17:19 -0300

    terminado

---
+++ script.js
@@ -0,0 +1,22 @@
+
+const form = document.getElementById('registro-form');
+    form.addEventListener('submit', (event) => {
+    event.preventDefault();
+const url = 'https://jsonplaceholder.typicode.com/users';
+const data = {
+    nombre: form.elements['nombre'].value,
+    apellido: form.elements['apellido'].value,
+    fechaNacimiento: form.elements['fecha-nacimiento'].value
+      };
+const options = {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+      };
+      fetch(url, options)
+        .then(response => response.json())
+        .then(data => console.log(data))
+        .catch(error => console.error(error));
+    });
